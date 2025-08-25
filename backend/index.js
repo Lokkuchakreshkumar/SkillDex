c42c5449110d1a1a9459bd0960eff1a95f6e046f
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-08-25 14:26:47 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://skilldex.onrender.com",
     credentials: true,
   })
 );
