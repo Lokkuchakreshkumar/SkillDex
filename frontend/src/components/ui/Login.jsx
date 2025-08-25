a5b0182893a502e921c328357672229181bcb0c4
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-08-25 14:55:53 +0530

    added redirecting

--- frontend/src/components/ui/Login.jsx
+++ frontend/src/components/ui/Login.jsx
@@ -3,12 +3,13 @@ import Nav from '../ui/Nav.jsx'
 import img from "../../assets/generated-image.png"
 
 const Login = () => {
+  let URL = import.meta.env.VITE_URL
   return (
     <div className='bg-black  min-h-screen w-full flex justify-center items-center'>
       <Nav/>
       <div className='w-full flex flex-col-reverse  justify-center sm:flex-row items-center  h-full'>
             
-                  <a href="http://localhost:8080/auth/google" className='p-4 text-sm text-wrap m-4 px-12 sm:w-[40%] w-[95%] sm:text-lg rounded-3xl h-[10%] text-slate-900 hover:shadow-lg hover:shadow-slate-800  transition duration-400 sora hover:animate-none  tracking-wider font-bold hover:-translate-y-0.5  google'>Continue with Google</a>
+                  <a href={`${URL}/auth/google`} className='p-4 text-sm text-wrap m-4 px-12 sm:w-[40%] w-[95%] sm:text-lg rounded-3xl h-[10%] text-slate-900 hover:shadow-lg hover:shadow-slate-800  transition duration-400 sora hover:animate-none  tracking-wider font-bold hover:-translate-y-0.5  google'>Continue with Google</a>
      
              
              <div  className='sm:w-[40%] w-[80%] flex justify-center items-center'>
