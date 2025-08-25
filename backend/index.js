b9f6130f2f9913ada29653b0b2c57a78a472ce6e
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-08-25 14:44:37 +0530

    callback changed for backend

--- backend/index.js
+++ backend/index.js
@@ -67,7 +67,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/auth/google/callback",
+      callbackURL: "https://skilldex.onrender.com/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       let newuser = await User.findOne({ googleId: profile.id });
