b3aca38691033b3f8e7f5d6f156773f8dd2caabf
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-08-26 23:24:16 +0530

    changed toast content

--- frontend/src/components/ui/Generate.jsx/Genmain.jsx
+++ frontend/src/components/ui/Generate.jsx/Genmain.jsx
@@ -181,7 +181,7 @@ else
   return (
     <div className="sm:flex w-full relative bg-[#0B0F14] sm:overflow-y-hidden min-h-screen sm:h-screen ">
       {
-        loading && !copy && toast('Generating your course (1–3 min). It’ll appear on your dashboard once ready.')
+        loading && !copy && toast('⏳ Generating your course (1–3 min). It’ll appear on your dashboard once ready.')
       }
      {
       copy && <div className="w-full">
@@ -408,7 +408,7 @@ else
   containerStyle={{}}
   toasterId="default"
   toastOptions={{
-    className: '',
+    className: 'pt-24',
     duration: 5000,
     removeDelay: 1000,
     style: {
