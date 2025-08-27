def4223c9cad95962bf6899e36cecf1b01c8b173
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-08-27 12:26:33 +0530

    added new manifest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,26 +9,34 @@ export default defineConfig({
   plugins: [react(),tailwindcss(),
       VitePWA({
       registerType: 'autoUpdate',
-      manifest: {
-        name: "SkillDex to define you",
-        short_name: "SkillDex",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#000000",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "../frontend/public/Adobe Express - file (1).png",
-            type: "image/png",
-            sizes: "192x192"
-          },
-          {
-            src: "../frontend/public/Adobe Express - file (1).png",
-            type: "image/png",
-            sizes: "512x512"
-          }
-        ]
-      }
+manifest: {
+  name: "SkillDex to define you",
+  short_name: "SkillDex",
+  start_url: "/",
+  scope: "/",
+  display: "standalone",
+  background_color: "#000000",
+  theme_color: "#000000",
+  icons: [
+    {
+      src: "/Adobe Express - file (1).png",
+      type: "image/png",
+      sizes: "192x192"
+    },
+    {
+      src: "/Adobe Express - file (1).png",
+      type: "image/png",
+      sizes: "512x512"
+    },
+    {
+      src: "/Adobe Express - file (1).png",
+      type: "image/png",
+      sizes: "512x512",
+      purpose: "maskable"
+    }
+  ]
+}
+
     })
   ],
 })
